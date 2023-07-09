d5581faf98f087abe7010495d9441e52d3b7bb89
Safayed Bin Islam <safayed.binislam@gmail.com>
2023-07-09 12:31:21 +0600

    Add files via upload

---
+++ maps.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <style>
+     #map {
+        height: 100%;
+      }
+
+  </style>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+  <link rel="stylesheet" href="css/common.css">
+  <title>Document</title>
+</head>
+
+<body>
+  <div class="container-fluid">
+    <div class="row">
+      <!--Navbar-->
+      <nav class="navbar navbar-expand-lg ">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#"><img src="images/logo.png" alt=""></a>
+
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="newsfeed.html">Newsfeed</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="maps.html">Maps</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="profile.html">Profile</a>
+              </li>
+
+            </ul>
+          </div>
+        </div>
+      </nav>
+      <!--Navbar-->
+    </div>
+  </div>
+  <div class="clr"></div>
+
+<div class="container">
+  <div class="row">
+    <div class="col-12">
+      <div id="map"> 
+
+      <script defer
+        src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCystrKYM7nUDZ4XOD_Ok55zVRSzfxhwuw&libraries=visualization&callback=initMap">
+    </script>
+
+      </div>
+    </div>
+  </div>
+</div>
+
+
+  <div class="clr"></div>
+
+
+
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
+  </script>
+</body>
+
+</html>l
